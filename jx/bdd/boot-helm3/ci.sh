a1d92e21e11da4701bbcbce4149eca6c81c500bc
James Strachan <james.strachan@gmail.com>
2020-04-06 19:46:47 +0100

    Update ci.sh

--- jx/bdd/boot-helm3/ci.sh
+++ jx/bdd/boot-helm3/ci.sh
@@ -59,8 +59,7 @@ echo "secrets:
 
 jxl boot secrets import -f /tmp/secrets.yaml --git-url `cat giturl.txt`
 
-# run the boot Job
-echo running: jxl boot run -b --git-url `cat giturl.txt`
+jxl version
 
 jxl boot run -b --job
 
